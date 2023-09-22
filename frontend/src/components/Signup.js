999c650e99f83e21b35a1f15873e900b340c117d
icodeix <sherwincarlosph@gmail.com>
2023-09-22 15:44:28 +0800

    compress image before upload

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -5,6 +5,8 @@ import "./styles/Signup.css";
 import { useNavigate } from "react-router-dom";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Compressor from 'compressorjs';
+
 const Signup = () => {
     const [createForm, setCreateForm] = useState({
         username: "",
@@ -21,12 +23,24 @@ const Signup = () => {
 
     const handleCreateFormChange = (e) => {
         const { name, value } = e.target;
+        const [compressedFile, setCompressedFile] = useState(null);
+        const image = e.target.files[0];
+       
+        new Compressor(image, {
+            quality: 0.8, // 0.6 can also be used, but its not recommended to go below.
+            success: (compressedResult) => {
+                // compressedResult has the compressed file.
+                // Use the compressed file to upload the images to your server.        
+                setCompressedFile(res)
+            },
+        });
+
 
         if (name === "profileImg") {
             setCreateForm(
                 {
                     ...createForm,
-                    [name]: e.target.files[0],
+                    [name]: compressedFile,
                 }
             )
         }
