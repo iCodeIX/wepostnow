80a203b152b08926a24535399153b2801462e296
icodeix <sherwincarlosph@gmail.com>
2023-09-22 15:47:26 +0800

    result

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -25,13 +25,13 @@ const Signup = () => {
         const { name, value } = e.target;
         const [compressedFile, setCompressedFile] = useState(null);
         const image = e.target.files[0];
-       
+
         new Compressor(image, {
             quality: 0.8, // 0.6 can also be used, but its not recommended to go below.
             success: (compressedResult) => {
                 // compressedResult has the compressed file.
                 // Use the compressed file to upload the images to your server.        
-                setCompressedFile(res)
+                setCompressedFile(compressedResult)
             },
         });
 
