e92cd74cb3ba84eb0948448e6b8bc27b7817efde
icodeix <sherwincarlosph@gmail.com>
2023-09-23 14:17:21 +0800

    create user header

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,6 +4,11 @@ const bcrypt = require("bcryptjs");
 
 
 async function createUser(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     const { username, email, password, gender } = req.body;
     const decodedPass = bcrypt.hashSync(password);
     const defaultAvatar = "https://res.cloudinary.com/df9i6l8cw/image/upload/v1695429568/userphoto/vuissdcq3kdpv9nd22ff.png";
