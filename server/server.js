f324faaf897beaba14c8caf21679cf23093deaf2
icodeix <sherwincarlosph@gmail.com>
2023-09-23 08:18:59 +0800

    cors origin

--- server/server.js
+++ server/server.js
@@ -15,16 +15,12 @@ const port = process.env.PORT || 3000;
 //create app of express
 const app = express();
 
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-app.use(cors(corsOptionsDelegate));
+app.use(cors({
+  origin: "*",
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  optionsSuccessStatus: 200
+
+}));
 
 
 app.use(express.json());
